7e9375fb805fbfd0c0010a7966a8b3a0016afb91
Okada <okhiro1207@gmail.com>
2025-11-26 17:20:39 +0900

    早くなれ

--- new_new_main.py
+++ new_new_main.py
@@ -518,9 +518,9 @@ async def get_root():
                     
                     mediaStream = await navigator.mediaDevices.getUserMedia({ 
                         audio: {
-                            echoCancellation: true,
-                            noiseSuppression: true,
-                            autoGainControl: true
+                            echoCancellation: false,
+                            noiseSuppression: false,
+                            autoGainControl: false
                         } 
                     });
                     
